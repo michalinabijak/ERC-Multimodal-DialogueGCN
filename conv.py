f8906c9a86e1e149d518c243ff826bddea14d2df
Michalina Bijak <michalina.bijak.20@ucl.ac.uk>
2021-07-18 23:55:53 +0200

    glove convert

---
+++ conv.py
@@ -0,0 +1,21 @@
+import json
+
+filename = "/Users/michalinabijak/PycharmProjects/erc_github/data/glove.6B.100d.txt"
+
+dict1 = {}
+
+with open(filename) as fh:
+  
+    for line in fh:
+  
+        # reads each line and trims of extra the spaces 
+        # and gives only the valid words
+        command, description = line.strip().split(None, 1)
+  
+        dict1[command] = description.strip()
+  
+# creating json file
+# the JSON file is named as test1
+out_file = open("glove_6B_100d.json", "w")
+json.dump(dict1, out_file, sort_keys = False)
+out_file.close()
